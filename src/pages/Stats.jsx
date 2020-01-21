cdc6b31d8183ea7bffc004bbe724a6b1014e13a2
Kimmy Lee <kimmy.lee17@gmail.com>
2020-01-20 23:43:35 -0500

    got /stats committed api to display

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,34 +1,61 @@
 import React from 'react'
+import moment from 'moment'
 import HeaderBar from '../components/HeaderBar'
 import TitleSection from '../components/TitleSection'
 import FloatingButtons from '../components/FloatingButtons'
+import * as colors from '../constants/colors'
+import strings from '../constants/strings';
 import routes from '../constants/routes'
 
 import './Stats.scss'
 
+
 class Stats extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      schedules: []
+    }
+    };
 
   componentWillMount() {
     let dataURL = 'https://cit-stats.herokuapp.com/services/confirmed';
-    return fetch(dataURL)
+    fetch(dataURL)
     .then(res => res.json())
     .then(res => {
-      console.log(res)
-  //    const schedule = res.map(val => {
-  //      const schedule = val.acf;
-  //      return {
-  //        people: schedule.people,
-  //        type: schedule.type,
-  //        createdAt: schedule.createdAt,
-  //        updatedAt: schedule.updatedAt
-  //      };
-  //    })
+//      console.log(res)
+
+     const schedules = res
+         .map((val, i) => ({
+           people: val.people,
+           type: val.type,
+           createdAt: val.createdAt,
+           updatedAt: val.updatedAt
+         }));
+     this.setState({
+       schedules
+     })
     })
+
   }
+
+    render() {
+      return <div><table>
+        {this.state.schedules.map((val, i) => {
+          return (
+          <tbody key={i}>
+            <tr>
+              <td>{JSON.stringify(val.people)}</td>
+              <td>{val.type}</td>
+              <td>{val.createdAt}</td>
+              <td>{val.updatedAt}</td>
+            </tr>
+          </tbody>
+          );
+        })}</table></div>
+    }
 }
-//  render() {
-//    content = 'text'
-//  }
+
 
 //  content = (
 //    <div className='stats-content'>
@@ -40,8 +67,6 @@ class Stats extends React.Component {
 //     </div>
 //   )
 //
-// }
-
-
+//}
 
 export default Stats
